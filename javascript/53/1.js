f54c19d6253a6d24225061e09afbbad1b7ea69df
NaftoliSimon <Naftolisimon97@gmail.com>
2020-07-03 11:42:58 -0400

    homework 53

---
+++ javascript/53/1.js
@@ -0,0 +1,5 @@
+'use strict'
+var name = 'your name';
+var email = 'your email';
+//window.alert('Warning! Blue Button Virus detected. Do not select or click on any blue buttons.');
+var age = prompt('your age?');
