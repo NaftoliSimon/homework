7044c78d8b65726dfc37cea62ed3394518caf2f4
NaftoliSimon <Naftolisimon97@gmail.com>
2020-09-13 17:00:11 -0400

    hw 66

---
+++ javascript/66/recipes.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    const pizza = $('#pizza');
+    const smores = $('#smores');
+    const ccPretzels = $('#ccPretzels');
+    const recipeDisplay = $('#recipeDisplay');
+    const recipeName = $('#recipeName');
+    const recipePhoto = $('#recipePhoto');
+    const ingredientsTitle = $('#ingredientsTitle');
+    let oldArraylength;
+
+    function callFetch(elem, file) {
+        elem.change(() => {
+            fetch(file)
+                .then(r => {
+                    if (!r.ok) {
+                        throw new Error(`${r.status} ${r.statusText}`);
+                    }
+                    return r.json();
+                })
+                .then(r => displayRecipe(r.name, r.photo, r.ingredients))
+                .catch(err => console.error(err));
+        });
+
+        function displayRecipe(name, photo, ingredients) {
+            clearIngredients();
+            ingredientsTitle.text('Ingredients');
+            recipeName.text(name);
+            recipePhoto.show();
+            recipePhoto.attr('src', photo);
+            for (let i = 0; i < ingredients.length; i++) {
+                recipeDisplay.append(`<p id="ingredient${i}">${ingredients[i]}</p>`);
+            }
+            oldArraylength = ingredients.length;
+        }
+        function clearIngredients() {
+            for (let i = 0; i < oldArraylength; i++) {
+                $(`#ingredient${i}`).remove();
+            }
+        }
+    }
+    callFetch(pizza, 'json/pizza.json');
+    callFetch(smores, 'json/smores.json');
+    callFetch(ccPretzels, 'json/ChocolateCoverdPretezels.json');
+
+}());
